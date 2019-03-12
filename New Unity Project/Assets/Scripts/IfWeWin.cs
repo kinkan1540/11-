35f6cf1a35e7786a4acbc2b8723099e333a29555
kinkan1540 <->
2019-03-12 11:41:38 +0900

    playerの変更

---
+++ New Unity Project/Assets/Scripts/IfWeWin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfWeWin : MonoBehaviour
+{
+    private CapsuleCollider collider;
+    //private GameObject player = GameObject.FindGameObjectWithTag("Player");
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+    }
+}
